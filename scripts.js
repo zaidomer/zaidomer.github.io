fdc573de4a97f78a2436110d1ed18e69ae556f6c
Zaid Omer <zaidomerone@gmail.com>
2019-12-28 16:42:15 -0500

    Contact Form Linked with Formspree

--- scripts.js
+++ scripts.js
@@ -13,3 +13,42 @@ function scrollFunction() {
     document.getElementsByClassName("navbar")[0].style.padding = "18px 20px";
   }
 }
+
+$(document).ready(function (){
+  $('.submit').click(function (event) {
+
+    var name = $('.name').val();
+    var email = $('.email').val();
+    var subject = $('.subject').val();
+    var message = $('.message').val();
+    document.getElementById("form_status").innerHTML = "";
+
+    if(name.length >= 1){
+      //document.getElementById("form_status").innerHTML += "Valid Name ";
+    }else{
+      event.preventDefault();
+      document.getElementById("form_status").innerHTML += "Invalid Name, ";
+    }
+
+    if(email.length > 5 && email.includes('@') && email.includes('.')){
+      //document.getElementById("form_status").innerHTML += "Valid Email ";
+    }else{
+      event.preventDefault();
+      document.getElementById("form_status").innerHTML += "Invalid Email, ";
+    }
+
+    if(subject.length >= 1){
+      //document.getElementById("form_status").innerHTML += "Valid Subject ";
+    }else{
+      event.preventDefault();
+      document.getElementById("form_status").innerHTML += "Invalid Subject, ";
+    }
+
+    if(message.length >= 5){
+      //document.getElementById("form_status").innerHTML += "Valid Message ";
+    }else{
+      event.preventDefault();
+      document.getElementById("form_status").innerHTML += "Invalid Message ";
+    }
+  })
+})
